eec1dcff2b1c7d669e7724257f02fbb68ba6965c
shubhachaudhary <shubha.learning@gmail.com>
2018-10-03 11:11:30 +1000

    sync master

--- 24_card_game.rb
+++ 24_card_game.rb
@@ -1,13 +1,20 @@
 class Card
     def initialize(suit, rank)
-        # Create a new card
+        @ suit= suit
+        @rank = rank
     end
     
     def face_card?
-        # Is the card a face card (above 10)?
-    end 
-    
+    face_card=[10,'J','Q','K','A']
+    face_card.include? @ rank 
+    p"card is face_card"
+    else
+    p " card is not a face_card" 
+    end
     def to_s
         # Return human readable card
     end
 end
+
+my_card=Card.new("spade","A")
+p my_card.face_card
