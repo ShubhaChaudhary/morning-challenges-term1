d7769881eeb39b1f561c02b5f3e9efe2e47f838f
shubhachaudhary <shubha.learning@gmail.com>
2018-10-04 12:27:33 +1000

    sync master

--- 24_card_game.rb
+++ 24_card_game.rb
@@ -1,20 +1,21 @@
 class Card
     def initialize(suit, rank)
-        @ suit= suit
+        @suit= suit
         @rank = rank
     end
+    def face_card
+     ['J','Q','K'].include?(@rank)
+    end  
     
-    def face_card?
-    face_card=[10,'J','Q','K','A']
-    face_card.include? @ rank 
-    p"card is face_card"
-    else
-    p " card is not a face_card" 
-    end
-    def to_s
-        # Return human readable card
+    def to_string
+     " #{@rank} of #{@suit}"
     end
 end
 
-my_card=Card.new("spade","A")
-p my_card.face_card
+my_card=Card.new("spades",'10')
+if my_card.face_card == true
+p "Card is face_card"
+else 
+p "card is not a face_card"
+end
+ p my_card.to_string
--- 26_cipher.rb
+++ 26_cipher.rb
@@ -12,17 +12,50 @@
 # If something in the string is not in the alphabet (e.g. punctuation, spaces), simply leave it as is.
 #
 # The shift will always be in range of [1, 26].
+# alphabet=('A'..'Z')
+# alphabet=alphabet.to_a
+# word="BFK"
+# final_word=word.split(//)
+# p alphabet
+# p final_word
+# p final_word.include?('B')
+# decoder=[]
+# alphabet.map do |x|
+# if final_word.include?(x)
+#   decoder << alphabet[alphabet.index(x)-5]
+# end
+# end
+# p decoder.join
 
 class CaesarCipher
   def initialize(shift)
-    #your code here
+    @shift=shift
+    @de_code=[]
+    @en_code=[]
+    @words=('A'..'Z').to_a
+    p @words.length
+    # @alphabet=@words.to_a
   end
-
   def encode(string)
-    #your code here
+    @string= string
+    @my_string=@string.split(//)
+    @my_string.map do |word|  
+      if @words.index(word)+@shift > 26
+        @final_index =@words.index(word)+@shift-26
+      else 
+        @final_index=@words.index(word)+@shift 
+      end
+        @en_code << @words[@final_index] 
+      end 
   end
-  
   def decode(string)
-    #your code here
+    @string= string
+    @my_string=@string.split(//)
+    @my_string.map{|word|  @de_code <<@words[@words.index(word)-@shift]}
+    @de_code  
   end
 end
+
+cipher=CaesarCipher.new(5)
+p cipher.decode("BFKKQJX")
+p cipher.encode("WAFFLES")
